8f7dbe6707b0a7940ee7b0dfce736b49b9242b5f
jesseSalonen <jesse.salonen@tuni.fi>
2021-10-27 11:53:52 +0300

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
+/*
+routes = controllers:
+hoitavat clienteiltä tulevat pyynnöt eteenpäin
+toimivat välikäsinä modelssien ja viewien välillä
+*/
+
+// Haetaan express käyttöön
 const express = require('express')
+// Haetaan expressin router-ominaisuus tähän käyttöön
 const router = express.Router()
 
+// Rakennetaan router GET-prosessille, joka siis hakee halutun viewin clientille, kun tämä avaa root endpointin '/'
 router.get('/', (req, res) => {
+    // Tämä viedään suoraan layout.ejs -tiedoston <body> -kenttään
     res.render('index')
 })
 
+// jaetaan router sovelluksen käyttöön, tätä kutsutaan server.js -tiedostossa, eli 'sovellustiedostossa'
 module.exports = router
--- server.js
+++ server.js
@@ -1,32 +1,47 @@
+// Haetaan ympäristömuuttuja .env-tiedostosta vain siinä tapauksessa,
+// jos sovellus ajetaan kehitystilassa (npm run devStart, ei npm run start)
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
+// Sisällytä kaikki asennetut paketit
 const express = require('express')
-const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
 
+// Käynnistä express sovellus, sisällytä se muuttujaan app
+const app = express()
+
+// Hae eri endpointeille tehdyt routerit muuttujiin
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 
-
+// Määritä, mitä view engineä käytetään, tässä tapauksessa ejs
 app.set('view engine', 'ejs')
+// Määritä, mistä sovellus saa viewinsä, tässä tapauksessa 'tämä_kansio/views'
+// Viewit hoitavat kaiken renderöintiin liittyvän
 app.set('views', __dirname + '/views')
+// Määritä, mistä sovellus saa layouttinsa, 'layouts/layout'
 app.set('layout', 'layouts/layout')
+// Käsketään sovellusta käyttämään asennettua express-ejs -layouttia
 app.use(expressLayouts)
+// Kerrotaan sovellukselle, mistä kaikki julkiset tiedostot löytyvät
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+app.use(express.urlencoded({ limit: '10mb', extended: false}))
 
+// Sisällytä mongoose-paketti, joka mahdollistaa mongoDB-tietokannan yhdistämisen
 const mongoose = require('mongoose')
+// Yhdistä mongoDB-tietokantaan, URL saadaan .env-tiedostosta
 mongoose.connect(process.env.DATABASE_URL, {
 useNewUrlParser: true })
+// Hallitse tietokantayhteyttä db-muuttujan takaa
 const db = mongoose.connection
+// Tulosta ilmoitukset virheestä tai yhdistämisestä konsoliin
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+// Käske sovellusta käyttämään aikaisemmin haettuja routereita tietyissä endpointeissa
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 
+// Määritetään portti, jota sovellus kuuntelee. Joko palvelimen määrittämä, tai oletuksena 3000
 app.listen(process.env.PORT || 3000)
 
